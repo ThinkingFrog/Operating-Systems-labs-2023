d759a232ec29f36a756351fca134b32ecd970220
ThinkingFrog <khripunkovdv@gmail.com>
2023-12-24 14:16:34 +0300

    Add simultaneous read-write tests

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,4 +1,5 @@
 #include "scsp_queue.hpp"
+#include <cassert>
 #include <iostream>
 #include <pthread.h>
 
@@ -21,8 +22,8 @@ void *read_from_queue(void *queue) {
 int main(int argc, char **argv) {
     SCSPQueue queue;
 
-    pthread_t writer_thread_1, writer_thread_2;
-    pthread_t reader_thread_1, reader_thread_2;
+    pthread_t writer_thread_1, writer_thread_2, writer_thread_3;
+    pthread_t reader_thread_1, reader_thread_2, reader_thread_3;
 
     pthread_create(&writer_thread_1, NULL, write_to_queue, &queue);
     pthread_create(&writer_thread_2, NULL, write_to_queue, &queue);
@@ -38,7 +39,17 @@ int main(int argc, char **argv) {
     pthread_join(reader_thread_1, NULL);
     pthread_join(reader_thread_2, NULL);
 
+    assert(queue.empty());
+
     std::cout << "Readers tests are complete" << std::endl;
+
+    pthread_create(&writer_thread_3, NULL, write_to_queue, &queue);
+    pthread_create(&reader_thread_3, NULL, read_from_queue, &queue);
+
+    pthread_join(writer_thread_3, NULL);
+    pthread_join(reader_thread_3, NULL);
+
+    std::cout << "Simultaneous read and write tests are complete" << std::endl;
 
     return 0;
 }
